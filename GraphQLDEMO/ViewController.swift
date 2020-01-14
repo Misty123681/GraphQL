80c4c58846687c3feab528cf723829a5d93f6b40
“Pooja <pooja.neosofttech.com>
2020-01-14 14:14:58 +0530

    Initial Commit

---
+++ GraphQLDEMO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GraphQLDEMO
+//
+//  Created by Neosoft on 14/01/20.
+//  Copyright © 2020 Neosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
